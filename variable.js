91854a824108e357f81374217aa2a113d175ecd3
hyeyeon-lee <63793866+hyeyeon-lee@users.noreply.github.com>
2020-06-19 17:28:14 +0900

    use variable
    let 개념 익히기

---
+++ variable.js
@@ -0,0 +1,13 @@
+// 1. Use strict
+// added in ES 5
+// use this for Valina Javascript.
+'use strict';
+console.log('Hello world');
+
+// 2. Variable
+// let (added in ES6)
+
+let name = 'hyeyeon';
+console.log(name);
+name = 'hello';
+console.log(name);
