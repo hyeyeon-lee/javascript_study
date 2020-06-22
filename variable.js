5551a5fea5196c2141e1eaad4150f38b7184724d
hyeyeonlee <dear.yeonee@gmail.com>
2020-06-22 23:17:09 +0900

    use variable
    const 익히기

--- variable.js
+++ variable.js
@@ -4,6 +4,7 @@
 "use strict";
 console.log("Hello world");
 console.log(age);
+
 // 2. Variable
 // let (added in ES6)
 //  global: 프로그램 시작에서 끝까지 메모리 탑재 - 최소한으로 쓰는 것이 좋음
@@ -29,3 +30,14 @@ console.log(age);
   var age;
 }
 console.log(age);
+
+// 3. Constants
+// 선언한 후엔 변경할 수 없음 - immutable
+// favor immutable data type always for a few reasons:
+// 웬만하면 값 할당 후 변경되지 않는 경우 사용
+// - security
+// - thread safety
+// - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+// mutable - let / immutable - const
