4ccc4e59e2c4fc4ce8126cfc6da58a5c877b9f4b
hyeyeonlee <dear.yeonee@gmail.com>
2020-07-08 23:07:15 +0900

    operator
    1. string 연결
    2. 계산
    3. 전치, 후치

---
+++ operator.js
@@ -0,0 +1,31 @@
+// 1. String concatenation
+console.log("my" + " cat");
+console.log("1" + 2);
+console.log(`string literals: 
+...
+1+2 = ${1 + 2}`);
+
+console.log("hyeyeon's book");
+
+// 2. Numeric operator
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
+
+// 3. Increment and decrement operator
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
+const preDecrement = --counter;
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
+const postDecrement = counter--;
+console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
