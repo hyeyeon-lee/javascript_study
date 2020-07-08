ac238007c2707003606a9f5f567d232205c45def
hyeyeonlee <dear.yeonee@gmail.com>
2020-07-09 00:01:33 +0900

    operator
    1. 할당
    2. 비교
    3. 논리

--- operator.js
+++ operator.js
@@ -29,3 +29,48 @@ const preDecrement = --counter;
 console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
 const postDecrement = counter--;
 console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y; // x = x+y;
+x -= y;
+x *= y;
+x /= y;
+
+// 5. Comparison Operators
+console.log(10 < 6); // less than
+console.log(10 <= 6); // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
+
+// 6. Logical operators: || (or), && (and), ! (not)
+const value1 = true;
+const value2 = 4 < 2;
+
+// || (or), finds the first truthy value
+// 처음 true를 만나면 멈추니,
+// < < < 뒤로갈수록 복잡한 것을 놓기
+// 처음부터 복잡한 것 두지 않기
+console.log(`or: ${value1 || value2 || check()}`);
+
+// && (and), finds the first falsy value
+// or와 동일, heavy 한 operation일수록 제일 뒤에서 처리
+console.log(`and: ${value1 || value2 || check()}`);
+
+// often used to compress long if-statement
+// nummableObject && nullableObject.something
+if (nummableObject != null) {
+  nummableObject.something;
+}
+
+// ! (not)
+console.log(!value);
+
+function check() {
+  for (let i = 0; i < 10; i++) {
+    // wasting time
+    console.log("No.....");
+  }
+  return true;
+}
