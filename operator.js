0d73df1964d2e3ed75610e2c005f6c39d2c72804
hyeyeonlee <dear.yeonee@gmail.com>
2020-07-18 14:38:59 +0900

    operator
    연습문제 풀이

--- operator.js
+++ operator.js
@@ -182,5 +182,42 @@ for (let i = 0; i < 10; i++) {
 
 // break, continue
 // Q1. iterate from 0 to 10 and print only even numbers (use continue)
+for (let i = 0; i < 10; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+/* 
+for (let i = 0; i < 11; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+=> 원할때만 출력하도록
+
+for (let i = 0; i < 11; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+*/
 
 // Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break)
+for (let i = 0; i < 10; i++) {
+  if (i === 8) {
+    break;
+  }
+  console.log(i);
+}
+
+/* 
+for (let i = 0; i < 11; i++) {
+  if (i > 8) {
+    break;
+  }
+  console.log(i);
+} */
