8e22064aff86fa324d6e51f1c9ed2f4b97ca67a4
hyeyeonlee <dear.yeonee@gmail.com>
2020-07-26 23:14:51 +0900

    function
    parameter가 있는 함수 만들어보기

--- function.js
+++ function.js
@@ -13,3 +13,20 @@ function printHello() {
   console.log("Hello");
 }
 printHello();
+
+// 자바스크립트 특성 상 타입이 정해져 있지 않아
+// 함수만 보고는 파라미터, 리턴 타입을 알 수 없음
+function log(message) {
+  console.log(message);
+}
+
+log("hyeyeon hello!!!");
+log(1234);
+
+// typescript 연습은 https://www.typescriptlang.org/
+/* 
+function log(message: string): number {
+  console.log(message);
+  return 0;
+}
+ */
