54ceb2797b67eac9dcd6c46f2eae9aa34c5918ad
hyeyeonlee <dear.yeonee@gmail.com>
2020-08-05 23:09:27 +0900

    class
    getter & setter

--- class.js
+++ class.js
@@ -23,3 +23,32 @@ const hyeyeon = new Person("hyeyeon", 20);
 console.log(hyeyeon.name);
 console.log(hyeyeon.age);
 hyeyeon.speak();
+
+// 2. Getter and Setter
+// 클래스를 잘못 사용해도 조금더 방어적으로 사용할 수 있도록
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get age() {
+    // return this.age;
+    return this._age;
+  }
+
+  set age(value) {
+    // setter 무한 호출 Maximum call stack size exceeded
+    // this.age = value;
+
+    /* if (value < 0) {
+      throw Error("age can not be negative");
+    } */
+
+    this._age = value < 0 ? 0 : value;
+  }
+}
+
+const user1 = new User("Steve", "Job", -1);
+console.log(user1.age);
