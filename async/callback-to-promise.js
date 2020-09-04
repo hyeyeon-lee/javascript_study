a5c6de6695371495e1c1fbeb5a1ce8932a2e69b6
hyeyeon-lee <dear.yeonee@gmail.com>
2020-09-04 21:08:37 +0900

    code 재정렬

--- async/callback-to-promise.js
+++ async/callback-to-promise.js
@@ -1,29 +1,28 @@
 class UserStorage {
   loginUser(id, password) {
-	  return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (
-			  (id === "hyeyeon" && password === "dream") ||
-			  (id === "coder" && password === "academy")
-			) {
-				resolve(id);
-			} else {
-				reject(new Error("not found"));
-			}
-		  }, 2000);
-	  })
-    
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === "hyeyeon" && password === "dream") ||
+          (id === "coder" && password === "academy")
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error("not found"));
+        }
+      }, 2000);
+    });
   }
   getRoles(user) {
-	  return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			if (user === "hyeyeon") {
-				resolve({ name: "hyeyeon", role: "admin" });
-			} else {
-				reject(new Error("no access"));
-			}
-		  });
-	  })
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "hyeyeon") {
+          resolve({ name: "hyeyeon", role: "admin" });
+        } else {
+          reject(new Error("no access"));
+        }
+      });
+    });
   }
 }
 
@@ -31,15 +30,16 @@ const userStorage = new UserStorage();
 const id = prompt("enter your id");
 const password = prompt("enter your password");
 
-userStorage.loginUser(id,password)
-.then(
-	(user) => {
-		userStorage.getRoles(user)
-		.then((userWithRole) => {
-			alert(
-			  `Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
-			);
-		  })
-		.catch(console.log);
-	})
-.catch(console.log);
+userStorage
+  .loginUser(id, password)
+  .then((user) => {
+    userStorage
+      .getRoles(user)
+      .then((userWithRole) => {
+        alert(
+          `Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+        );
+      })
+      .catch(console.log);
+  })
+  .catch(console.log);
