2ebd11bfbce4abc18cd0046a5f38f3ccd9dcd5c2
hyeyeon-lee <dear.yeonee@gmail.com>
2020-09-09 21:38:05 +0900

    미니게임 숙제 (클릭)

--- javascript_game/script.js
+++ javascript_game/script.js
@@ -1,22 +1,49 @@
 "use strict"; // es5로 엄격하게 해주세요
 
-const clothes = data;
+let clothes = data;
+setData(clothes);
 
-const clothesDiv = document.querySelector("#clothes");
-clothes.forEach((item) => {
-  //item
-  let itemDiv = document.createElement("div");
-  itemDiv.setAttribute("class", "item");
+function setData(data) {
+  const clothesDiv = document.querySelector("#clothes");
+  // 비우기
+  clothesDiv.innerHTML = "";
+  data.forEach((item) => {
+    //item
+    let itemDiv = document.createElement("div");
+    itemDiv.setAttribute("class", "item");
 
-  // item > img
-  let image = document.createElement("img");
-  image.setAttribute("src", `imgs/${item.image}`);
-  // item > p
-  let text = document.createElement("p");
-  let detail = document.createTextNode(`${item.gender}, ${item.size} size`);
+    // item > img
+    let image = document.createElement("img");
+    image.setAttribute("src", `imgs/${item.image}`);
+    // item > p
+    let text = document.createElement("p");
+    let detail = document.createTextNode(`${item.gender}, ${item.size} size`);
 
-  text.appendChild(detail);
-  itemDiv.appendChild(image);
-  itemDiv.appendChild(text);
-  clothesDiv.appendChild(itemDiv);
+    text.appendChild(detail);
+    itemDiv.appendChild(image);
+    itemDiv.appendChild(text);
+    clothesDiv.appendChild(itemDiv);
+  });
+}
+
+const category = document.querySelectorAll(".category *");
+category.forEach((item) => {
+  item.addEventListener("click", sortClothes);
 });
+
+function sortClothes(e) {
+  const target = e.target;
+  const targetId = target.id;
+  const targetClass = target.className;
+
+  let result = [];
+  if (targetClass === "type") {
+    // filter, find 헷갈림
+    // arrow function {} 헷갈림
+    result = clothes.filter((item) => targetId === item.type);
+  } else {
+    result = clothes.filter((item) => targetId === item.color);
+  }
+
+  setData(result);
+}
